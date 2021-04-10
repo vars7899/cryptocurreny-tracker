afcb0012784dbbfefe0b7b8dd9bacf6e10901964
vars7899 <9728300615vars7899@users.noreply.github.com>
2021-04-10 09:57:40 -0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Coin from "./Coin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
+    useEffect(() => {
+        axios
+            .get(
+                "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+            )
+            .then((res) => {
+                setCoins(res.data);
+                console.log(res.data);
+            })
+            .catch((err) => {
+                window.alert("Not able to fetch data");
+            });
+    }, []);
+
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const filteredCoins = coins.filter((coin) =>
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return (
+        <div className="coin-app">
+            <div className="coin-search">
+                <h1 className="coin-text">Search a currency</h1>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="search"
+                        className="coin-input"
+                        onChange={handleChange}
+                    />
+                </form>
+            </div>
+            {filteredCoins.map((coin) => {
+                return (
+                    <Coin
+                        key={coin.id}
+                        name={coin.name}
+                        image={coin.image}
+                        symbol={coin.symbol}
+                        volume={coin.total_volume}
+                        price={coin.current_price}
+                        priceChange={coin.price_change_percentage_24h}
+                        marketcap={coin.market_cap}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
